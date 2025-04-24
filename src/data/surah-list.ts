01be26c6187bd5918fc7c8bf7477def66b6eb7c8
Iqbal <hai.balbaal@gmail.com>
2025-04-24 15:34:41 +0700

    chore: setup Prettier for code formatter

---
+++ src/data/surah-list.ts
@@ -0,0 +1,20 @@
+import { SurahList } from "@/types/surah";
+
+const surahList: SurahList = [
+  {
+    number: "1",
+    name: "الفاتحة",
+    name_latin: "Al-Fatihah",
+    translation_name: "Pembukaan",
+    number_of_ayah: "7",
+  },
+  {
+    number: "2",
+    name: "البقرة",
+    name_latin: "Al-Baqarah",
+    translation_name: "Sapi",
+    number_of_ayah: "286",
+  },
+];
+
+export default surahList;
---
+++ src/types/surah.ts
@@ -0,0 +1,9 @@
+export type SurahItem = {
+  number: string
+  name: string
+  name_latin: string
+  translation_name: string
+  number_of_ayah: string
+}
+
+export type SurahList = SurahItem[];
