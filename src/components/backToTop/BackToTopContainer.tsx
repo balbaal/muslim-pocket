2df2dd54dcd0fb61d4f6c833fcdb419744a83f09
balbaal <hai.balbaal@gmail.com>
2025-05-29 14:02:28 +0800

    Feat/back to top (#6)
    * feat: back to top scroll button
    
    * fix: remove unused code
    
    * fix: show button when spesific position

--- src/components/backToTop/BackToTopContainer.tsx
+++ src/components/backToTop/BackToTopContainer.tsx
@@ -1,16 +1,16 @@
 "use client";
 
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import BackToTopView from "./BackToTopView";
 
 const BackToTopContainer = () => {
-  const refScrollPosition = useRef<number>(0);
+  const [currentScroll, setCurrentScroll] = useState<number>(0);
 
   useEffect(() => {
-    const handleCurrentScroll = () => (refScrollPosition.current = window.scrollY);
+    const handleCurrentScroll = () => setCurrentScroll(window.scrollY);
     window.addEventListener("scroll", handleCurrentScroll);
 
-    return window.removeEventListener("scroll", handleCurrentScroll);
+    return () => window.removeEventListener("scroll", handleCurrentScroll);
   }, []);
 
   const handleScrollTop = () => {
@@ -20,8 +20,10 @@ const BackToTopContainer = () => {
     });
   };
 
+  if (currentScroll < 10000) return null;
+
   return (
-    <div className="fixed z-50 right-4 bottom-4 bg-orange-500 rounded-full p-3 flex items-center justify-center">
+    <div className="fixed z-50 right-4 bottom-4 bg-orange-500 rounded-full p-3 flex items-center justify-center transition-all duration-500 animate-slide-up">
       <BackToTopView handleOnClick={handleScrollTop} />
     </div>
   );
