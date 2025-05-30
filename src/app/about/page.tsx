155fe6cdd8eb41bd7b391b52c6879284b3c39c46
balbaal <hai.balbaal@gmail.com>
2025-05-30 19:37:20 +0800

    refactor: change to client side the page (#9)

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const about = () => {
+  return (
+    <div className="w-full p-4">
+      <h1 className="text-lg font-bold text-black">Tentang...</h1>
+    </div>
+  );
+};
+
+export default about;
