40954aaaa39b18052eb1e8e73785bc5591e37a60
Iqbal <hai.balbaal@gmail.com>
2025-06-01 01:15:06 +0700

    feat: prepare integrate API

--- src/components/prayer/PrayerFetcher.tsx
+++ src/components/prayer/PrayerFetcher.tsx
@@ -1,4 +1,1 @@
-/**
- * curl -X GET "https://api.aladhan.com/v1/calendar/2025/5?latitude=-5.152472&longitude=119.465431&method=20&shafaq=general" \
- -H 'accept: application/json' 
-*/
+// fetcher that will call query hooks with react query
