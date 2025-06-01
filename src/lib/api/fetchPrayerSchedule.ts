8777d65fa206378e8952eb95add9715081d61b2d
balbaal <hai.balbaal@gmail.com>
2025-06-01 20:48:47 +0800

    feat: get lat long position (#12)

--- src/lib/api/fetchPrayerSchedule.ts
+++ src/lib/api/fetchPrayerSchedule.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
+import { getCurrentLatLng } from "../utils";
 
 export const fetchPrayerSchedule = async () => {
+  const position = await getCurrentLatLng();
+
   const result = await axios.get(
-    "https://api.aladhan.com/v1/calendar/2025/6?latitude=-6.183717&longitude=106.830582&method=20&shafaq=general"
+    `https://api.aladhan.com/v1/calendar/2025/6?latitude=${position.lat}&longitude=${position.lng}&method=20&shafaq=general`
   );
 
   return result.data;
