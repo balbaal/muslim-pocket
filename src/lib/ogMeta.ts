24a14dee86093c567a124fabc7b11237244e4b5f
Iqbal <hai.balbaal@gmail.com>
2025-06-04 15:25:50 +0700

    feat: set image og for socmed

--- src/lib/ogMeta.ts
+++ src/lib/ogMeta.ts
@@ -23,5 +23,13 @@ export const createOGMeta: createOGMetaType = ({
     url,
     type,
     siteName,
+    images: [
+      {
+        url: "/og-muslim-pocket-fancy.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Banner Muslim Pocket",
+      },
+    ],
   };
 };
