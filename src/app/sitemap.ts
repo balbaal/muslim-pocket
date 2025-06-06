b2578ab6b561433f0cae95c2aa7af7a8d24b3273
balbaal <hai.balbaal@gmail.com>
2025-06-07 02:22:42 +0800

    Fix/sitemap robot format (#38)
    * fix: sitemap robot format
    
    * fix: dynamic sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,36 +1,29 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: "https://muslimpocket.id",
-      lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.8,
-    },
-    {
-      url: "https://muslimpocket.id/jadwal-sholat",
-      lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.8,
-    },
-    {
-      url: "https://muslimpocket.id/semua-surah",
-      lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.8,
-    },
-    {
-      url: "https://muslimpocket.id/tentang",
-      lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.8,
-    },
-    {
-      url: "https://muslimpocket.id/tasbih-digital",
-      lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.8,
-    },
+  const baseUrl = "https://muslimpocket.id";
+  const TOTAL_SURAH = 114;
+
+  const surahUrls = Array.from({ length: TOTAL_SURAH }, (_, i) => {
+    const nomor = i + 1;
+    return `${baseUrl}/surah/${nomor}`;
+  });
+
+  const staticUrls = [
+    `${baseUrl}`,
+    `${baseUrl}/jadwal-sholat`,
+    `${baseUrl}/semua-surah`,
+    `${baseUrl}/tentang`,
+    `${baseUrl}/tasbih-digital`,
   ];
+
+  const allUrls = [...staticUrls, ...surahUrls];
+  const results: MetadataRoute.Sitemap = allUrls.map((url) => ({
+    url,
+    lastModified: new Date(),
+    changeFrequency: "daily",
+    priority: 0.8,
+  }));
+
+  return [...results];
 }
