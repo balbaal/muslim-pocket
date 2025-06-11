43f7232a5145971835fb702f4222478efc698ab9
balbaal <hai.balbaal@gmail.com>
2025-06-12 01:47:16 +0800

    refactor: prayer schedule method get api/local storage (#53)

--- src/components/prayer/PrayerFetcher.tsx
+++ src/components/prayer/PrayerFetcher.tsx
@@ -1,16 +1,24 @@
 "use client";
 
+import { useEffect } from "react";
+import { usePathname } from "next/navigation";
 import { useQueryPrayerSchedule } from "@/hooks/useQueryPrayerSchedule";
 import { PrayerDate, usePrayerStore } from "@/store/prayerStore";
 import { PrayerScheduleResponse } from "@/types/prayerSchedule";
-import { useEffect } from "react";
+import {
+  getPrayerSchedule as getPrayerScheduleStorage,
+  setPrayerSchedule as setPrayerScheduleStorage,
+} from "@/lib/storage";
 
 const PrayerFetcher = () => {
-  const { data, isLoading, error } = useQueryPrayerSchedule();
+  const { refetch } = useQueryPrayerSchedule();
+  const prayerScheduleState = usePrayerStore((state) => state.prayerSchedule);
   const setPrayerSchedule = usePrayerStore((state) => state.setPrayerSchedule);
   const setNextPrayer = usePrayerStore((state) => state.setNextPrayer);
   const setTimeZone = usePrayerStore((state) => state.setTimeZone);
   const setIsloadingPrayer = usePrayerStore((state) => state.setIsloadingPrayer);
+
+  const pathname = usePathname();
 
   const handleNextPrayer = (result: PrayerDate[]): void => {
     const date = new Date();
@@ -44,6 +52,14 @@ const PrayerFetcher = () => {
 
   const mappingPrayerSchedule = (data: PrayerScheduleResponse["data"]) => {
     const allowedPrayerName = ["Fajr", "Dhuhr", "Asr", "Maghrib", "Isha"];
+    const mappingNameIndonesia: Record<string, string> = {
+      Fajr: "Subuh",
+      Dhuhr: "Dzuhur",
+      Asr: "Ashar",
+      Maghrib: "Maghrib",
+      Isha: "Isya",
+    };
+
     const timeZone = data[0]?.meta?.timezone || "";
     const result = data.map(
       (z): PrayerDate => ({
@@ -62,7 +78,7 @@ const PrayerFetcher = () => {
             const timeString = `${z.date.gregorian.year}-${String(z.date.gregorian.month.number).padStart(2, "0")}-${z.date.gregorian.day}T${time}:00`;
 
             return {
-              name: key,
+              name: mappingNameIndonesia[key] || "-",
               time: time,
               timestamps: String(new Date(timeString).getTime()),
             };
@@ -75,14 +91,39 @@ const PrayerFetcher = () => {
     handleNextPrayer(result);
   };
 
-  useEffect(() => {
-    if (!isLoading && !error) {
-      setIsloadingPrayer(true);
-      mappingPrayerSchedule(data?.data || []);
-      setIsloadingPrayer(false);
+  const handleTriggerRefetch = async () => {
+    const { data } = await refetch();
+    const prayerData = data?.data || [];
+
+    mappingPrayerSchedule(prayerData);
+    if (prayerData?.length > 0) {
+      setPrayerScheduleStorage(prayerData);
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isLoading]);
+  };
+
+  useEffect(() => {
+    const prayerSchedule = getPrayerScheduleStorage();
+    const storagePrayerMonth = prayerSchedule?.[0]?.date?.gregorian?.month?.number;
+    const currentMonth = new Date().getMonth() + 1;
+
+    const isSameMonth = storagePrayerMonth === currentMonth;
+
+    if (prayerSchedule?.length > 0 && prayerScheduleState?.length > 0 && isSameMonth) return;
+
+    setIsloadingPrayer(true);
+
+    if (prayerSchedule?.length > 0 && isSameMonth) {
+      mappingPrayerSchedule(prayerSchedule);
+      setIsloadingPrayer(false);
+      return;
+    }
+
+    if (pathname === "/jadwal-sholat") {
+      handleTriggerRefetch();
+    }
+
+    setIsloadingPrayer(false);
+  });
 
   return null;
 };
