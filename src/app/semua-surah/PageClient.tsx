da5c9b62e15608e0bcb4d6b40ee6784d2eaf37e7
Iqbal <hai.balbaal@gmail.com>
2025-06-17 14:02:57 +0700

    refactor: remove unnecessary process infinite load

--- src/app/semua-surah/PageClient.tsx
+++ src/app/semua-surah/PageClient.tsx
@@ -7,38 +7,15 @@ import Icon from "@/components/Icons";
 import { surahList } from "@/data/surah-list";
 import Breadcrumb from "@/components/Breadcrumb";
 import generateBreadcrumb from "@/lib/generateBreadcrumb";
-import { useCallback, useState, useEffect } from "react";
+import { useCallback, useState } from "react";
 import { SurahItemPreview } from "@/types/surah";
 import { clearString } from "@/lib/transformString";
 import { debounce } from "@/lib/utils";
 import ListView from "./ListView";
-import { useInView } from "react-intersection-observer";
-
-const ITEMS_PER_PAGE = 5;
 
 const PageClient = () => {
-  const [filteredSurahs, setFilteredSurahs] = useState<SurahItemPreview[]>(surahList);
-  const [displayedSurahs, setDisplayedSurahs] = useState<SurahItemPreview[]>([]);
-  const [search, setSearch] = useState<string>("");
-
+  const [displayedSurahs, setDisplayedSurahs] = useState<SurahItemPreview[]>(surahList);
   const pathname = usePathname();
-
-  const { ref, inView } = useInView({
-    threshold: 0,
-    rootMargin: "100px",
-  });
-
-  useEffect(() => {
-    if (inView) {
-      const nextItems = filteredSurahs.slice(
-        displayedSurahs.length,
-        displayedSurahs.length + ITEMS_PER_PAGE
-      );
-      if (nextItems.length > 0) {
-        setDisplayedSurahs((prev) => [...prev, ...nextItems]);
-      }
-    }
-  }, [inView, filteredSurahs, displayedSurahs.length]);
 
   const handleSearch = useCallback((search: string) => {
     const newFilteredList = surahList.filter(
@@ -48,9 +25,7 @@ const PageClient = () => {
           .toLowerCase()
           .includes(clearString(search).toLowerCase())
     );
-    setSearch(search);
-    setFilteredSurahs(newFilteredList);
-    setDisplayedSurahs(newFilteredList.slice(0, ITEMS_PER_PAGE));
+    setDisplayedSurahs(newFilteredList);
   }, []);
 
   const debouncedSearch = debounce(handleSearch, 500);
@@ -58,8 +33,6 @@ const PageClient = () => {
   const onHandleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     debouncedSearch(e.target.value);
   };
-
-  const hasMore = displayedSurahs.length < filteredSurahs.length;
 
   return (
     <div className="flex flex-col gap-6">
@@ -73,12 +46,7 @@ const PageClient = () => {
         />
       </div>
       <div className="flex flex-col gap-2">
-        <ListView surahList={displayedSurahs} isSearching={!!search} />
-        {hasMore && (
-          <div ref={ref} className="py-4 text-center">
-            <p className="text-gray-500">Memuat surah lainnya...</p>
-          </div>
-        )}
+        <ListView surahList={displayedSurahs} />
       </div>
     </div>
   );
