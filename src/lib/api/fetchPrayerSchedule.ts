77ed46334baf7addca68c48444cd405da76e8f61
balbaal <hai.balbaal@gmail.com>
2025-07-01 21:30:16 +0800

    hotfix: get dynamic payload prayer schedule (#62)

--- src/lib/api/fetchPrayerSchedule.ts
+++ src/lib/api/fetchPrayerSchedule.ts
@@ -3,9 +3,11 @@ import { getCurrentLatLng } from "../utils";
 
 export const fetchPrayerSchedule = async () => {
   const position = await getCurrentLatLng();
+  const currentMonth = new Date().getMonth() + 1;
+  const currentYear = new Date().getFullYear();
 
   const result = await axios.get(
-    `https://api.aladhan.com/v1/calendar/2025/6?latitude=${position.lat}&longitude=${position.lng}&method=20&shafaq=general`
+    `https://api.aladhan.com/v1/calendar/${currentYear}/${currentMonth}?latitude=${position.lat}&longitude=${position.lng}&method=20&shafaq=general`
   );
 
   return result.data;
